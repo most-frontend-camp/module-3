3012bb727fb100d2841340f4e10e1336f7e66a6d
Beibarys Jmart <bumirbayev@jmart.kz>
2024-06-21 12:30:03 +0500

    update

--- notes-4-functions/example.js
+++ notes-4-functions/example.js
@@ -1,12 +1,11 @@
-function power(a, n) {
-    if( n === 0) {
-        return 1;
-    }
-    return a**n;
-    // return a*power(a, n - 1);
-}
+// IIFE
+(function() {
+    let n = 10;
+    console.log("12123 World!");
+    console.log(n + " asdasdasd");
+})()
 
 
-console.log(power(2,0)); // 1
-console.log(power(2,2)); // 4
-console.log(power(2,4)); // 16
+(function (){
+
+})()
